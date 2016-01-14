e95df7331809c0e0ad48d16012b22d53e675cc0b
liubei <544599899@qq.com>
2016-01-14 13:50:35 +0800

    注释read.js

--- wuliu1/util/read.js
+++ wuliu1/util/read.js
@@ -2,8 +2,8 @@ var originalRequest=require('request');
 var cheerio=require('cheerio');
 var debug=require('debug')('ddstwl:update:read');
 
-//@param {http://www.ddstwl.com/index.php} url
-//@param {} callback
+@param {http://www.ddstwl.com/index.php} url
+@param {} callback
 
 function request(url, callback){
 	originalRequest(url,callback);
